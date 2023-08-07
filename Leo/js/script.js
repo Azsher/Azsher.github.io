a4b22b7d6458fefce28d3727bc96a7e5e6821d5b
Oscar Antayhua <108460315+Azsher@users.noreply.github.com>
2023-08-07 14:18:28 -0500

    Delete Leo directory

--- Leo/js/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-menu.onclick = () => {
-    menu.classList.toggle('ri-close-line');
-    navbar.classList.toggle('open');
-};
